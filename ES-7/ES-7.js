638dd1551f3b72bb4d814ce97cbbd03adf572a2c
Vaibhav Dharmik <95203957+vaibhavDharmik1@users.noreply.github.com>
2022-02-18 22:52:09 +0530

    Added ES-7 Features

---
+++ ES-7/ES-7.js
@@ -0,0 +1,15 @@
+// Exponentiation Operator
+// The exponentiation operator (**) raises the first operand to the power of the second operand.
+// let x = 5;
+// let z = x ** 2;          // result is 25
+// .........................................................
+
+// exponentiation assignment
+// The exponentiation assignment operator (**=) raises the value of a variable to the power of the right operand.
+// let x = 5;
+// let z = Math.pow(x,2);   // result is 25
+// ........................................................
+// JavaScript Array.includes()
+// ECMAScript 2016 introduced Array.prototype.includes to arrays. This allows us to check if an element is present in an array:
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.includes("Mango"); // is true
