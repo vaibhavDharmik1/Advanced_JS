832472dc5d478f10c492ed8f95c1f8bec6c7de29
Vaibhav Dharmik <95203957+vaibhavDharmik1@users.noreply.github.com>
2022-02-20 20:58:45 +0530

    Added: basic knowledge of HTTP and HTTPS

---
+++ http.js
@@ -0,0 +1,19 @@
+// The World Wide Web is about communication between web clients and web servers.
+// Clients are often browsers, but they can be any type of program or device.
+// Servers are most often computers in the cloud.
+// HTTP is called a stateless protocol because each command is executed independently, 
+// without any knowledge of the commands that came before it.
+
+// A similar abbreviation, HTTPS means Hypertext Transfer Protocol Secure.
+//  Basically, it is the secure version of HTTP. 
+// Communications between the browser and website are encrypted by Transport Layer Security (TLS), 
+// or its predecessor, Secure Sockets Layer (SSL).
+// HTTP uses port 80
+// HTTPS uses port 443
+
+// Status Code of HTTP
+// 1xx = 100 to 199 (Information code)
+// 2xx = 200 to 299 (Successfully Message code)
+// 3xx = 300 to 399 (Redirectional code)
+// 4xx = 400 to 499 (Client side error code)
+// 5xx = 500 to 599 (server side error code)
